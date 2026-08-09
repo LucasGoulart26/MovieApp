bf71ae2a3f155664762828915d95e4ce5eba8a40
LucasGoulart24 <Goulart7720@gmail.com>
2026-08-08 21:43:00 -0300

    filme

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@ import { useState } from "react";
 import { filmes } from "./data/filmes";
 import { novosfilmes } from "./data/novosFilmes";
 import { RiMovie2Line } from "react-icons/ri";
+import { FaSearch } from "react-icons/fa";
 import capa from "./assets/capa.png";
 
 function App() {
   const[active, setActive] = useState("All");
+  const[avaliacao] = useState(5);
+
+  // Estrelas de avaliação
+  const estrelaAvaliacao = (nota: number) => {
+    const estrelas = [];
+    for(let i = 1; i <= 5; i++) {
+      if(i <= nota) {
+        estrelas.push(<span className="text-2xl text-amber-300">★</span>);
+      }else {
+        estrelas.push(<span className="text-2xl text-amber-300">☆</span>);
+      }
+    }
+    return estrelas
+  }
 
   const buttons = [
     "All",
@@ -25,10 +40,10 @@ function App() {
   : filmes.filter((filme) => filme.categoria.toLowerCase().includes(active.toLowerCase()))
   return(
     <div className="w-full h-screen bg-zinc-800">
-      <div className="w-full h-20 flex items-center justify-between p-3 bg-zinc-900">
+      <div className="w-full h-20 flex items-center justify-between p-5 bg-zinc-900">
         <h1 className="text-3xl text-amber-300">MovieApp</h1>
         <div className="w-96 h-11 flex items-center justify-center">
-          <input type="text" placeholder="Search movies" className="w-full h-full outline-0 bg-zinc-800 border-2 border-zinc-700 p-3 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl"/>
+          <input type="text" placeholder="Search movies" className="w-full h-full outline-0 bg-zinc-700 border-2 border-zinc-600 p-3 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl"/>
           <button className="w-20 h-full cursor-pointer rounded-br-lg rounded-tr-lg bg-amber-300 active:bg-amber-400 transition-all duration-300">Search</button>
         </div>
       </div>
@@ -37,6 +52,7 @@ function App() {
         <div className="Card w-2xl h-96 flex items-start justify-items-start flex-col gap-3">
           <h1 className="text-3xl text-white">Demon Slayer: Kimetsu no Yaiba - Castelo Infinito</h1>
           <h2 className="text-2xl text-white">2025 - Ação/Aventura</h2>
+          <h3 className="text-2xl text-white">Avaliação: {estrelaAvaliacao(avaliacao)}</h3>
           <p className="text-white text-justify">Os Pilares agora enfrentam Muzan e decidem atacá-lo juntos. No entanto, eles são transportados para a Fortaleza Infinita antes que possam desferir um único golpe e, portanto, são separados.</p>
           <div className="flex items-center justify-center gap-3">
             <button className="w-28 p-2 cursor-pointer rounded-lg bg-amber-300 active:bg-amber-400 transition-all duration-300">Assistir</button>
@@ -72,19 +88,29 @@ function App() {
             <p className="text-zinc-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
           </div>
           {novosfilmes.map((novosfilme, index) => (
-            <div key={index} className="w-full h-20 p-5 rounded-lg bg-zinc-800 border-2 border-zinc-700 flex items-center justify-center gap-3 hover:border-amber-300 transition-all duration-300">
+            <div key={index} className="w-full h-24 p-5 rounded-lg bg-zinc-800 border-2 border-zinc-700 flex items-center justify-center gap-3 hover:border-amber-300 transition-all duration-300">
               <div className="w-12 h-12 rounded-lg bg-amber-300 flex items-center justify-center">
                 <RiMovie2Line className="text-3xl"/>
               </div>
               <div className="w-full flex items-start justify-center flex-col">
                 <div className="w-full flex items-center justify-between">
                   <h2 className="text-2xl text-zinc-200">{novosfilme.titulo}</h2>
-                  <p className="w-20 text-center p-1 rounded-lg bg-amber-300">{novosfilme.ano}</p>
+                  <p className="w-24 text-center p-1 rounded-lg bg-amber-300">{novosfilme.data}</p>
                 </div>
-                <button className="cursor-pointer text-zinc-200 hover:text-amber-300 transition-all duration-300">Watch trailer</button>
               </div>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="w-full h-screen flex items-center justify-center flex-col bg-zinc-900">
+        <div className="w-[90%] h-20 flex items-center justify-between p-4 border-l-2 border-l-amber-300">
+          <h1 className="text-3xl text-amber-300">Cinema</h1>
+          <div className="flex items-center justify-center p-2 bg-zinc-700 border-2 border-zinc-600 rounded-2xl">
+            <input type="text" placeholder="Search movie" className="w-96 h-11 outline-0 placeholder:text-white text-white"/>
+            <button className="w-11 h-11 rounded-full cursor-pointer bg-amber-300 active:bg-amber-400 transition-all duration-300 flex items-center justify-center">
+              <FaSearch className="text-2xl"/>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/data/novosFilmes.ts
+++ src/data/novosFilmes.ts
@@ -1,18 +1,14 @@
 export const novosfilmes = [
   {
-    titulo: "Titulo",
-    ano: "2026",
+    titulo: "A Odisseia",
+    data: "16/07/2026",
   },
   {
-    titulo: "Titulo",
-    ano: "2026",
+    titulo: "Homem-Aranha: Um Novo Dia ",
+    data: "29/07/2026",
   },
   {
-    titulo: "Titulo",
-    ano: "2026",
-  },
-  {
-    titulo: "Titulo",
-    ano: "2026",
+    titulo: "O Fim da Rua",
+    data: "13/08/2026",
   },
 ];
